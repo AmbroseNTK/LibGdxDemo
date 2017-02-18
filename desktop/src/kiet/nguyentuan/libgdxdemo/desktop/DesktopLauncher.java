2f77f87eed3d1fa050184d671da98d2d44b1f092
kiettuannguyen <nguyentuankiet23031998@gmail.com>
2017-02-18 22:25:05 +0700

    Init new project

---
+++ desktop/src/kiet/nguyentuan/libgdxdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kiet.nguyentuan.libgdxdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kiet.nguyentuan.libgdxdemo.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
