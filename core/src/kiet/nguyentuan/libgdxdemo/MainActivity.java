73f8b46d6271526eee01c8bf09577a17221a860f
kiettuannguyen <nguyentuankiet23031998@gmail.com>
2017-02-18 23:43:31 +0700

    Create class BaseActor extends Actor and use Stage object

--- core/src/kiet/nguyentuan/libgdxdemo/MainActivity.java
+++ core/src/kiet/nguyentuan/libgdxdemo/MainActivity.java
@@ -8,77 +8,88 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MainActivity extends ApplicationAdapter {
-	SpriteBatch batch;
+
+	Stage mainStage;
+
 	Texture mouseTexture;
 	Texture cheeseTexture;
 	Texture floorTexture;
 	Texture winTexture;
 
-	Sprite mouse;
-	Sprite cheese;
-	Sprite floor;
-	Sprite winPanel;
+	BaseActor mouse;
+	BaseActor cheese;
+	BaseActor floor;
+	BaseActor winPanel;
 
 	boolean win;
 
 	Rectangle cheeseRec;
 	Rectangle mouseRec;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 		mouseTexture=new Texture("mouse.png");
 		cheeseTexture=new Texture("cheese.png");
 		floorTexture=new Texture("tiles.jpg");
 		winTexture=new Texture("you-win.png");
-		mouse=new Sprite(mouseTexture);
+
+		mouse=new BaseActor(mouseTexture);
 		mouse.setPosition(20,20);
-		cheese=new Sprite(cheeseTexture);
+		cheese=new BaseActor(cheeseTexture);
 		cheese.setPosition(400,300);
-		floor=new Sprite(floorTexture);
+		floor=new BaseActor(floorTexture);
 		floor.setPosition(0,0);
-		winPanel=new Sprite(winTexture);
+		winPanel=new BaseActor(winTexture);
 		winPanel.setPosition(170,60);
+		winPanel.setVisible(false);
+
+		mainStage=new Stage();
+		mainStage.addActor(floor);
+		mainStage.addActor(cheese);
+		mainStage.addActor(mouse);
+		mainStage.addActor(winPanel);
+
 		win=false;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.8f,0.8f,1,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		float dT=Gdx.graphics.getDeltaTime();
+		mainStage.act(dT);
 		checkKeyboard();
 		checkWin();
-		batch.begin();
-		floor.draw(batch);
-		cheese.draw(batch);
-		mouse.draw(batch);
+		mainStage.draw();
 		if(win)
-			batch.draw(winTexture,170,60);
-		batch.end();
+			winPanel.setVisible(true);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
 		mouseTexture.dispose();
 		cheeseTexture.dispose();
 		floorTexture.dispose();
 		winTexture.dispose();
 	}
 	private void checkKeyboard(){
+		mouse.velocity=new Vector2(0,0);
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
-			mouse.translateX(-1);
+			mouse.velocity.set(mouse.velocity.x-200,mouse.velocity.y);
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-			mouse.translateX(1);
+			mouse.velocity.set(mouse.velocity.x+200,mouse.velocity.y);
 		if(Gdx.input.isKeyPressed(Input.Keys.UP))
-			mouse.translateY(1);
+			mouse.velocity.set(mouse.velocity.x,mouse.velocity.y+200);
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
-			mouse.translateY(-1);
+			mouse.velocity.set(mouse.velocity.x,mouse.velocity.y-200);
 	}
 	private void checkWin(){
-		cheeseRec=cheese.getBoundingRectangle();
-		mouseRec=mouse.getBoundingRectangle();
+		cheeseRec=cheese.getBoundingRectange();
+		mouseRec=mouse.getBoundingRectange();
 		if(cheeseRec.contains(mouseRec))
 			win=true;
 	}
