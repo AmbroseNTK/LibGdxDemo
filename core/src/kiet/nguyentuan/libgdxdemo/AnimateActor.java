496694d40bc135a996e223a946683c6b4869f39c
kiettuannguyen <nguyentuankiet23031998@gmail.com>
2017-03-02 14:24:03 +0700

    Add PhysicsActor

--- core/src/kiet/nguyentuan/libgdxdemo/AnimateActor.java
+++ core/src/kiet/nguyentuan/libgdxdemo/AnimateActor.java
@@ -4,8 +4,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g3d.particles.influencers.RegionInfluencer;
 import com.badlogic.gdx.math.MathUtils;
+
+import java.util.HashMap;
 
 /**
  * Created by kiettuannguyen on 18/02/2017.
@@ -14,9 +15,49 @@ import com.badlogic.gdx.math.MathUtils;
 public class AnimateActor extends BaseActor {
     public float elapseTime;
     public Animation animation;
+    private String activeName;
+    private Animation activeAnim;
+    private HashMap<String,Animation> animationHashMap;
     public AnimateActor(){
         super();
         elapseTime=0;
+        activeAnim=null;
+        activeName=null;
+        animationHashMap=new HashMap<String, Animation>();
+    }
+    public void storageAnimtion(String name, Animation anim){
+        animationHashMap.put(name, anim);
+        if(activeAnim==null){
+            setActiveAnimation(name);
+        }
+    }
+    public void storageAnimation(String name, Texture tex)
+    {
+        TextureRegion reg = new TextureRegion(tex);
+        TextureRegion[] frames = { reg };
+        Animation anim = new Animation(1.0f, frames);
+        storageAnimtion(name, anim);
+    }
+    public void setActiveAnimation(String name)
+    {
+        if ( !animationHashMap.containsKey(name) )
+        {
+            System.out.println("No animation: " + name);
+            return;
+        }
+        // no need to set animation if already running
+        if ( activeName.equals(name) )
+            return;
+        activeName = name;
+        activeAnim = animationHashMap.get(name);
+        elapseTime = 0;
+        Texture tex=(Texture)animation.getKeyFrame(0);
+        setWidth( tex.getWidth() );
+        setHeight( tex.getHeight() );
+    }
+    public String getAnimationName()
+    {
+        return activeName;
     }
     public AnimateActor(Texture texture){
         super(texture);
@@ -37,4 +78,5 @@ public class AnimateActor extends BaseActor {
         textureRegion.setRegion((TextureRegion) animation.getKeyFrame(elapseTime));
         super.draw(batch,parentAlpha);
     }
+
 }
--- core/src/kiet/nguyentuan/libgdxdemo/BaseActor.java
+++ core/src/kiet/nguyentuan/libgdxdemo/BaseActor.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.compression.lzma.Base;
 
 /**
  * Created by kiettuannguyen on 18/02/2017.
@@ -16,37 +20,90 @@ public class BaseActor extends Actor {
     public TextureRegion textureRegion;
     public Rectangle boundary;
     public Vector2 velocity;
-    public BaseActor(){
+    public Polygon boundingPolygon;
+
+    public BaseActor() {
         super();
-        textureRegion=new TextureRegion();
-        boundary=new Rectangle();
-        velocity=new Vector2(0,0);
+        textureRegion = new TextureRegion();
+        boundary = new Rectangle();
+        velocity = new Vector2(0, 0);
+        boundingPolygon = null;
     }
-    public BaseActor(Texture texture){
+
+    public BaseActor(Texture texture) {
         super();
-        textureRegion=new TextureRegion();
-        boundary=new Rectangle();
-        velocity=new Vector2(0,0);
+        textureRegion = new TextureRegion();
+        boundary = new Rectangle();
+        velocity = new Vector2(0, 0);
+        boundingPolygon = null;
         setTexture(texture);
     }
-    public void setTexture(Texture texture){
+
+    public void setTexture(Texture texture) {
         setWidth(texture.getWidth());
         setHeight(texture.getHeight());
         textureRegion.setRegion(texture);
     }
-    public Rectangle getBoundingRectange(){
-        boundary.set(getX(),getY(),getWidth(),getHeight());
+
+    public Rectangle getBoundingRectange() {
+        boundary.set(getX(), getY(), getWidth(), getHeight());
         return boundary;
     }
-    public void act(float dT){
-        super.act(dT);
-        moveBy(velocity.x*dT,velocity.y*dT);
-    }
-    public void draw(Batch batch, float parentAlpha){
-        Color c=getColor();
-        batch.setColor(c.r,c.g,c.b,c.a);
-        if(isVisible())
-            batch.draw(textureRegion,getX(),getY(),getOriginX(),getOriginY(),getWidth(),getHeight(),getScaleX(),getScaleY(),getRotation());
 
+    public void act(float dT) {
+        super.act(dT);
+        moveBy(velocity.x * dT, velocity.y * dT);
+    }
+
+    public void draw(Batch batch, float parentAlpha) {
+        Color c = getColor();
+        batch.setColor(c.r, c.g, c.b, c.a);
+        if (isVisible())
+            batch.draw(textureRegion, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+
+    }
+
+    public void setRectangleBoundary() {
+        float w = getWidth();
+        float h = getHeight();
+        float[] vertices = {0, 0, w, 0, w, h, 0, h};
+        boundingPolygon = new Polygon();
+        boundingPolygon.setOrigin(getOriginX(), getOriginY());
+    }
+
+    public Polygon getBoundingPolygon() {
+        boundingPolygon.setPosition(getX(), getY());
+        boundingPolygon.setRotation(getRotation());
+        return boundingPolygon;
+    }
+
+    public boolean overlaps(BaseActor other, boolean resolve) {
+        Polygon polygon1 = this.getBoundingPolygon();
+        Polygon polygon2 = other.getBoundingPolygon();
+        if (!polygon1.getBoundingRectangle().overlaps(polygon2.getBoundingRectangle()))
+            return false;
+        Intersector.MinimumTranslationVector minimumTranslationVector = new Intersector.MinimumTranslationVector();
+        boolean polyOverlap = Intersector.overlapConvexPolygons(polygon1, polygon2, minimumTranslationVector);
+        if (polyOverlap && resolve)
+            this.moveBy(minimumTranslationVector.normal.x * minimumTranslationVector.depth, minimumTranslationVector.normal.y * minimumTranslationVector.depth);
+        float significant = 0.5f;
+        return (polyOverlap && (minimumTranslationVector.depth > significant));
+    }
+    public void copy(BaseActor original){
+        this.textureRegion=new TextureRegion(original.textureRegion);
+        if(original.boundingPolygon !=null){
+            this.boundingPolygon=new Polygon(original.getBoundingPolygon().getVertices());
+            this.boundingPolygon.setOrigin(original.getOriginX(),getOriginY());
+        }
+        this.setPosition(original.getX(),original.getY());
+        this.setOrigin(original.getOriginX(),original.getOriginY());
+        this.setSize(original.getWidth(),original.getHeight());
+        this.setColor(original.getColor());
+        this.setVisible(original.isVisible());
+    }
+    public BaseActor clone(){
+        BaseActor newBie=new BaseActor();
+        newBie.copy(this);
+        return newBie;
     }
 }
